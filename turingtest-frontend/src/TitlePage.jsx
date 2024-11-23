e4f7014109709779cb91a6941309dc7889543e13
SzupanBurliga <szburliga@gmail.com>
2024-11-23 19:16:03 +0100

    google tłumacz

--- turingtest-frontend/src/TitlePage.jsx
+++ turingtest-frontend/src/TitlePage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-
 function TitlePage() {
     const navigate = useNavigate();
 
@@ -9,15 +8,17 @@ function TitlePage() {
         <div className="title-page">
             <div className="title-content">
                 <h1>Are You Human?</h1>
-                <h3>Chat with someone for two minutes,
-                    and try to figure out if it was a fellow human or an AI bot.</h3>
-                <h3>Think you can tell the difference?</h3>
-                <button onClick={() => navigate("/MainView")}>Go to Chat</button>
-                <button onClick={() => navigate("/TuringTest")}>About Turing Test</button>
+                <h3>
+                    Porozmawiaj z kimś przez dwie minuty
+                    i spróbuj ustalić, czy był to inny człowiek, czy bot AI.
+                </h3>
+                <h3>Myślisz, że potrafisz to odróżnić?</h3>
+                <button onClick={() => navigate("/MainView")}>Przejdź do czatu</button>
+                <button onClick={() => navigate("/TuringTest")}>O Teście Turinga</button>
             </div>
         </div>
-
     );
 }
 
 export default TitlePage;
+
