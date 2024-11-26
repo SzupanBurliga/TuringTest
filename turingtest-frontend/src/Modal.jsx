9a77a8661d203ab860c0accec01170453e1d71f9
At1z <Adrianlistkiewiczv2@gmail.com>
2024-11-27 00:03:58 +0100

    Modal wyświetla czy odpowiedź jest poprawna

--- turingtest-frontend/src/Modal.jsx
+++ turingtest-frontend/src/Modal.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Modal.css";
 
 const Modal = ({ title, message, closeModal, onVote, room }) => {
   const navigate = useNavigate();
+  const [showResult, setShowResult] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(false);
+
   const handleVote = (vote) => {
     const isCorrect =
       (["room1", "room3", "room5"].includes(room) && vote === "AI Bot") ||
       (["room2", "room4", "room6"].includes(room) && vote === "Human");
 
-    console.log("User voted: ", vote);
-    console.log("Was correct:", isCorrect);
-    onVote({ vote, isCorrect });
-    closeModal();
-    navigate("/TitlePage");
+    setIsCorrect(isCorrect);
+    setShowResult(true);
+
+    // Opóźniamy zamknięcie modalu i nawigację, aby użytkownik mógł zobaczyć wynik
+    setTimeout(() => {
+      onVote({ vote, isCorrect });
+      closeModal();
+      navigate("/TitlePage");
+    }, 2000);
   };
 
   return (
@@ -21,14 +28,24 @@ const Modal = ({ title, message, closeModal, onVote, room }) => {
       <div className="modal-content">
         <h2>{title}</h2>
         <p>{message}</p>
-        <div className="modal-buttons">
-          <button onClick={() => handleVote("AI Bot")} className="buttonAI">
-            AI
-          </button>
-          <button onClick={() => handleVote("Human")} className="buttonHuman">
-            Człowiek
-          </button>
-        </div>
+        {!showResult ? (
+          <div className="modal-buttons">
+            <button onClick={() => handleVote("AI Bot")} className="buttonAI">
+              AI
+            </button>
+            <button onClick={() => handleVote("Human")} className="buttonHuman">
+              Człowiek
+            </button>
+          </div>
+        ) : (
+          <div className="result-message">
+            {isCorrect ? (
+              <p style={{ color: "green" }}>Poprawna odpowiedź!</p>
+            ) : (
+              <p style={{ color: "red" }}>Niepoprawna odpowiedź!</p>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
